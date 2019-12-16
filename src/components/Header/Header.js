9ad6aec6e9a694a2af712a2e6c1a0ed8866211dd
traxx10 <chuksben16@gmail.com>
2019-12-16 16:09:10 +0100

    started project

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,32 +1,34 @@
 import React, { PureComponent } from "react";
+import Button from "@material-ui/core/Button";
 import styles from "./Header.module.scss";
 
 class Header extends PureComponent {
   render() {
     return (
       <header className={styles.Header}>
-        <div className={styles.image}>
-          <div className={styles.container}>
-            <svg
-              x="0px"
-              y="0px"
-              viewBox="0 0 3841 108.5"
-              className={styles.svg}
-              //   style={{ overflow: 'scroll', enableBackground: }}
-              //   xml:space="preserve"
-            >
-              <defs></defs>
-
-              <path
-                className={styles.fill}
-                d="M3360.5,97.739c-242,0-480-48.375-480-48.375
-                S2647.5,0.5,2400.5,0.5s-480,48.375-480,48.375s-238,48.864-480,48.864s-480-48.375-480-48.375S727.5,0.5,480.5,0.5
-                S0.5,48.875,0.5,48.875V108h1920h1920V48.875C3840.5,48.875,3602.5,97.739,3360.5,97.739z"
-              />
-            </svg>
-          </div>
+        <div className={styles.HeaderTop}>
+          {/* <Button variant="contained" color="primary">
+            Hello World
+          </Button> */}
         </div>
-        <div className={styles.white}></div>
+        <div className={styles.WavesContainer}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path
+              fill="#3746D5"
+              fill-opacity="1"
+              d="M0,96L40,80C80,64,160,32,240,48C320,64,400,128,480,133.3C560,139,640,85,720,85.3C800,85,880,139,960,149.3C1040,160,1120,128,1200,117.3C1280,107,1360,117,1400,122.7L1440,128L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"
+            ></path>
+          </svg>
+        </div>
+        {/* <div className={styles.WavesContainerSecond}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path
+              fill="#3746D5"
+              fill-opacity="0.5"
+              d="M0,96L40,80C80,64,160,32,240,48C320,64,400,128,480,133.3C560,139,640,85,720,85.3C800,85,880,139,960,149.3C1040,160,1120,128,1200,117.3C1280,107,1360,117,1400,122.7L1440,128L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"
+            ></path>
+          </svg>
+        </div> */}
       </header>
     );
   }
