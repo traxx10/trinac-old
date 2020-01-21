8b973ec022a6dc7e8d945b60e6d7f084fbfdf5fc
traxx10 <chuksben16@gmail.com>
2020-01-21 16:30:26 +0100

    added reviews

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -2,11 +2,13 @@ import React, { PureComponent } from "react";
 import { connect } from "react-redux";
 import ItemsCarousel from "react-items-carousel";
 import { Card, Grid, Avatar } from "@material-ui/core";
+import _ from "lodash";
+import StarRatings from "react-star-ratings";
 import styles from "./Reviews.module.scss";
 
 const noOfItems = 12;
 const noOfCards = 3;
-const autoPlayDelay = 2000;
+const autoPlayDelay = 2500;
 const chevronWidth = 40;
 
 class Reviews extends PureComponent {
@@ -36,10 +38,23 @@ class Reviews extends PureComponent {
 
   render() {
     const { reviews, dimension } = this.props;
-
+    const rating = _.meanBy(reviews, function(review) {
+      return review.rating;
+    });
     return (
       <div className={styles.Reviews}>
         <div className={styles.HeaderContainer}>
+          <div className={styles.StarContainer}>
+            <StarRatings
+              rating={rating}
+              starRatedColor="yellow"
+              starDimension={"25px"}
+              starEmptyColor="#fff"
+              // changeRating={this.changeRating}
+              numberOfStars={5}
+              name="rating"
+            />
+          </div>
           <h1>What our clients are saying!</h1>
         </div>
         <div className={styles.ContentContainer}>
@@ -49,7 +64,7 @@ class Reviews extends PureComponent {
               <div className={styles.SelectedReview}>
                 <ItemsCarousel
                   infiniteLoop={false}
-                  gutter={24}
+                  gutter={dimension.width <= 990 ? 24 : 34}
                   activePosition={"center"}
                   chevronWidth={60}
                   disableSwipe={false}
@@ -62,9 +77,32 @@ class Reviews extends PureComponent {
                   activeItemIndex={this.state.activeItemIndex}
                   requestToChangeActive={this.onChange}
                 >
-                  {reviews.map((_, i) => (
+                  {reviews.map((review, i) => (
                     <Card className={styles.Card}>
-                      <div key={i} />
+                      <div key={i} className={styles.CardInner}>
+                        <Avatar
+                          alt="Trinac Reviews"
+                          className={styles.Avatar}
+                          src={require("../../assets/reviews/fiverr.png")}
+                        />
+                        <div className={styles.CardReview}>
+                          <blockquote>
+                            <span>“{review.review}”</span>
+                          </blockquote>
+
+                          <div className={styles.CardDetails}>
+                            <blockquote>
+                              <span>{review.name}</span> <br />
+                            </blockquote>
+                          </div>
+
+                          <div className={styles.CardDesignation}>
+                            <blockquote>
+                              <span>CEO@{review.company}</span> <br />
+                            </blockquote>
+                          </div>
+                        </div>
+                      </div>
                     </Card>
                   ))}
                 </ItemsCarousel>
--- src/reducers/ReviewsReducer.js
+++ src/reducers/ReviewsReducer.js
@@ -6,61 +6,70 @@ const INITIAL_STATE = {
       review:
         "Ipsum duis eu adipisicing id veniam ex eu commodo anim laboris. Eu aliqua nisi do ullamco commodo laboris occaecat. Eu incididunt fugiat eiusmod duis aliquip labore ut minim ipsum. Non aute sunt aute sint velit est non cillum consectetur.",
       name: "Hello One",
-      company: "Aureans"
+      company: "Aureans",
+      rating: 4
     },
     {
       review:
-        "Dolore veniam ipsum exercitation nostrud. Minim magna consectetur esse adipisicing aliqua minim proident laboris. Non id dolor consectetur adipisicing ex qui veniam elit excepteur elit veniam ullamco eiusmod. Veniam eiusmod commodo amet nisi. Laboris pariatur in aliqua enim dolor nisi proident nostrud in. Adipisicing magna quis in do. Cupidatat non aute aute Lorem excepteur qui dolor.",
+        "Laboris exercitation ipsum labore minim est nulla consectetur esse anim deserunt anim non eu est.",
       name: "Hello Two",
-      company: "Aureans Two"
+      company: "Aureans Two",
+      rating: 4.6
     },
     {
       review:
-        "Adipisicing labore pariatur adipisicing fugiat cupidatat minim. Amet in enim enim esse cupidatat est qui nostrud veniam incididunt reprehenderit adipisicing qui qui. Dolore cillum non elit exercitation. Enim ipsum consequat in nulla ullamco irure anim fugiat sint occaecat ullamco enim nisi. Lorem aliquip cillum dolor eiusmod irure culpa ad. Lorem dolore elit do aliquip dolore nisi occaecat ipsum occaecat est incididunt tempor et consectetur. Adipisicing laboris irure sint ex nisi laborum mollit veniam magna consectetur consequat sint ut id.",
+        "Nostrud sunt proident mollit ea fugiat exercitation mollit elit incididunt nisi cupidatat nostrud laborum.",
       name: "Hello Three",
-      company: "Aureans Three"
+      company: "Aureans Three",
+      rating: 5
     },
     {
-      review:
-        "Cillum velit aliqua cillum dolore. Cillum excepteur deserunt ea duis adipisicing ex magna sunt dolor ea. Sit ut enim dolore fugiat ipsum amet in et sit do sunt enim id reprehenderit. Ipsum velit commodo nisi consectetur reprehenderit proident magna sint ex cupidatat sit Lorem. Ad non cillum ullamco proident excepteur in do.",
+      review: "Magna aliquip ullamco dolore do nostrud ad aute do veniam enim.",
       name: "Hello Four",
-      company: "Aureans Four"
+      company: "Aureans Four",
+      rating: 4.8
     },
     {
       review:
-        "Ullamco in ex pariatur nulla ea dolore quis adipisicing tempor labore ipsum ullamco velit. Anim mollit proident eu sunt eu sit labore ipsum occaecat magna minim in velit. Mollit exercitation nisi anim officia ipsum ullamco incididunt ipsum aute voluptate consectetur. Nulla laborum excepteur sunt tempor eu labore fugiat labore. Incididunt fugiat est incididunt laborum cillum fugiat. Cillum consectetur excepteur adipisicing amet cillum ipsum deserunt ad quis.",
+        "Ullamco in ex pariatur nulla ea dolore quis adipisicing tempor labore ipsum ullamco velit. Anim mollit proident eu sunt eu sit labore ipsum occaecat magna minim in velit. Mollit.",
       name: "Hello Five",
-      company: "Aureans Fiver"
+      company: "Aureans Fiver",
+      rating: 5
     },
     {
       review:
-        "Aute Lorem tempor do incididunt. Sint est id nisi sit. Proident dolore eiusmod nisi esse irure amet deserunt exercitation sit exercitation consectetur labore. Dolore irure minim labore aliqua exercitation incididunt tempor officia enim ex reprehenderit laboris labore ut. Ut sunt minim elit sint veniam.",
+        "Aute Lorem tempor do incididunt. Sint est id nisi sit. Proident dolore eiusmod nisi esse irure amet deserunt exercitation sit exercitation consectetur labore.",
       name: "Hello Six",
-      company: "Aureans Six"
+      company: "Aureans Six",
+      rating: 5
     },
     {
       review:
-        "Excepteur enim dolore voluptate irure exercitation ea dolor non tempor velit nostrud et commodo. Excepteur magna minim quis magna incididunt veniam est est dolor voluptate. Ullamco quis quis sint magna proident aliquip. Ea sit minim reprehenderit aliquip commodo nulla culpa voluptate quis voluptate. Exercitation non deserunt irure sunt labore labore qui proident laborum est dolor veniam in elit.",
+        "Excepteur enim dolore voluptate irure exercitation ea dolor non tempor velit nostrud et commodo. Excepteur magna minim quis magna incididunt veniam est est dolor voluptate.",
       name: "Hello Seven",
-      company: "Aureans Seven"
+      company: "Aureans Seven",
+      rating: 5
     },
     {
       review:
-        "Aute officia velit pariatur laborum id sit anim occaecat cillum. Nostrud sint cillum fugiat officia id ex do occaecat fugiat. Enim laborum ipsum pariatur proident dolor. Consequat ad consequat et fugiat aliquip mollit aliqua aliqua. Minim nisi duis eu dolor consectetur consequat labore dolore velit in Lorem ipsum.",
+        "Aute officia velit pariatur laborum id sit anim occaecat cillum. Nostrud sint cillum fugiat officia id ex do occaecat fugiat.",
       name: "Hello Eight",
-      company: "Aureans Eight"
+      company: "Aureans Eight",
+      rating: 4.7
     },
     {
       review:
-        "Elit nisi anim ad reprehenderit. Consequat do laboris ipsum officia ut do ipsum. Dolore tempor anim Lorem ad ullamco enim velit ullamco quis incididunt sint elit. Occaecat deserunt exercitation anim eu incididunt quis. Aute enim labore fugiat ullamco elit fugiat exercitation ullamco officia ad non nulla.",
+        "Elit nisi anim ad reprehenderit. Consequat do laboris ipsum officia ut do ipsum. Dolore tempor anim Lorem ad ullamco enim velit ullamco quis incididunt sint elit.",
       name: "Hello Nine",
-      company: "Aureans Nine"
+      company: "Aureans Nine",
+      rating: 4.8
     },
     {
       review:
-        "Excepteur ad sit eiusmod non pariatur dolore quis minim aliquip ea sunt velit. Occaecat anim excepteur qui qui voluptate cupidatat quis deserunt. Id laboris laboris est fugiat culpa aliqua officia esse deserunt laboris esse qui Lorem ipsum. Velit magna et ad non exercitation consectetur qui sit. Officia dolore occaecat ullamco voluptate nulla ad ea cillum magna esse.",
+        "Excepteur ad sit eiusmod non pariatur dolore quis minim aliquip ea sunt velit. Occaecat anim excepteur qui qui voluptate cupidatat quis deserunt. ",
       name: "Hello Ten",
-      company: "Aureans Ten"
+      company: "Aureans Ten",
+      rating: 5
     }
   ]
 };
