f66fc81dec184a6deb113188767fa31889070b0d
traxx10 <chuksben16@gmail.com>
2020-03-01 18:56:36 +0100

    added footer

--- src/components/Portfolio/PortfolioDetail.js
+++ src/components/Portfolio/PortfolioDetail.js
@@ -13,6 +13,7 @@ import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import { ThreeBounce } from "better-react-spinkit";
 import Img from "react-image";
 import styles from "./PortfolioDetail.module.scss";
+import Footer from "../Footer/Footer";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -35,57 +36,60 @@ function PortfolioDetail(props) {
   const { data } = props.location.state;
 
   return (
-    <div className={styles.PortfolioDetail}>
-      <AppBar style={{ backgroundColor: "#fff" }} position="static">
-        <Toolbar>
-          <IconButton
-            onClick={() => props.history.goBack()}
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <ArrowBackIcon style={{ fontSize: 25 }} />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            {`${data.title}`} 😎
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <div className={styles.Content}>
-        <Grid className={styles.GridContainer} container spacing={0}>
-          <Grid item xs={12} sm={12} md={6} lg={3} xl={3}></Grid>
-          <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-            <Card className={styles.Card}>
-              <div className={styles.ImageContainer}>
-                <Img
-                  loader={
-                    <div className={styles.LoaderContainer}>
-                      <ThreeBounce size={15} />
-                    </div>
-                  }
-                  src={data.url}
-                  className={styles.Image}
-                />
-              </div>
-              <div className={styles.ParagrahContainer}>
-                <p>
-                  Velit exercitation enim cupidatat sint. Ut consequat amet anim
-                  consequat minim ad ex culpa nulla magna ullamco eu. Occaecat
-                  magna deserunt dolore occaecat irure eu anim fugiat deserunt
-                  cillum mollit sint veniam labore. Velit exercitation enim
-                  cupidatat sint. Ut consequat amet anim consequat minim ad ex
-                  culpa nulla magna ullamco eu. Occaecat magna deserunt dolore
-                  occaecat irure eu anim fugiat deserunt cillum mollit sint
-                  veniam labore.
-                </p>
-              </div>
-            </Card>
+    <>
+      <div className={styles.PortfolioDetail}>
+        <AppBar style={{ backgroundColor: "#fff" }} position="static">
+          <Toolbar>
+            <IconButton
+              onClick={() => props.history.goBack()}
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <ArrowBackIcon style={{ fontSize: 25 }} />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              {`${data.title}`} 😎
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <div className={styles.Content}>
+          <Grid className={styles.GridContainer} container spacing={0}>
+            <Grid item xs={12} sm={12} md={6} lg={3} xl={3}></Grid>
+            <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+              <Card className={styles.Card}>
+                <div className={styles.ImageContainer}>
+                  <Img
+                    loader={
+                      <div className={styles.LoaderContainer}>
+                        <ThreeBounce size={15} />
+                      </div>
+                    }
+                    src={data.url}
+                    className={styles.Image}
+                  />
+                </div>
+                <div className={styles.ParagrahContainer}>
+                  <p>
+                    Velit exercitation enim cupidatat sint. Ut consequat amet
+                    anim consequat minim ad ex culpa nulla magna ullamco eu.
+                    Occaecat magna deserunt dolore occaecat irure eu anim fugiat
+                    deserunt cillum mollit sint veniam labore. Velit
+                    exercitation enim cupidatat sint. Ut consequat amet anim
+                    consequat minim ad ex culpa nulla magna ullamco eu. Occaecat
+                    magna deserunt dolore occaecat irure eu anim fugiat deserunt
+                    cillum mollit sint veniam labore.
+                  </p>
+                </div>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={12} md={6} lg={3} xl={3}></Grid>
           </Grid>
-          <Grid item xs={12} sm={12} md={6} lg={3} xl={3}></Grid>
-        </Grid>
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 
